ce13f79f017a93d52fb107b1e3334cf1a551d9a7
asgardpepa <josefpospisil@seznam.cz>
2021-04-18 09:34:03 +0200

    exercise

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let palette = {
+    colors: ["blue", "yellow", "red", "green", "black", "white"]
+};
+
+let hasManyColors = palette.colors.length > 3;
+
+console.log("more than 3 colors on palette: " + hasManyColors);
+
